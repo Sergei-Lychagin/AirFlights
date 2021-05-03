66a5fac56851ca8297c0aba3bec6fd9c5a80205e
Sergei Lychagin <lychagin0@gmail.com>
2021-05-03 11:56:08 +0400

    класс comparator + поиск

--- src/main/java/ru/netology/manager/Manager.java
+++ src/main/java/ru/netology/manager/Manager.java
@@ -4,6 +4,7 @@ import ru.netology.domain.Ticket;
 import ru.netology.repository.Repository;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Manager {
     private Repository repository;
@@ -23,6 +24,19 @@ public class Manager {
             }
         }
         Arrays.sort(result);
+        return result;
+    }
+    public Ticket[] findAll(String from, String to, Comparator<Ticket> comparator) {
+        Ticket[] result = new Ticket[0];
+        for (Ticket offer : repository.getAll()) {
+            if (offer.getFrom().equalsIgnoreCase(from) && offer.getTo().equalsIgnoreCase(to)) {
+                Ticket[] tmp = new Ticket[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = offer;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result,comparator);
         return result;
     }
 }
